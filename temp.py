d258aad323953601962d736990fae31c391f9fa5
Kirat1708 <70762687+Kirat1708@users.noreply.github.com>
2020-09-05 22:17:51 +0530

    Session 1

---
+++ temp.py
@@ -0,0 +1,240 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print
+
+Kirat=10
+
+print(Kirat)
+
+print("Kirat Chhabra")
+
+print("Vikas", "Khullar",sep ="***",end ="    ")
+print("Vikas", "Khullar",sep ="***")
+
+help(print)
+
+help("OPERATORS")
+
+!python --version
+
+!pip list
+
+county=input("Your Country")
+
+input("Your Country")
+
+a=10
+
+b="Kirat"
+
+c=20.22
+
+print(county)
+
+type(a)
+
+type(b)
+
+type(c)
+
+type(s)
+county=input("Your Country")
+
+print(county)
+
+Phonenum=int(input())
+type(Phonenum)
+print(Phonenum)
+
+country= "India"
+
+print("I live in",country, "which is a very good place")
+print("I live in {0} which is a very good place".format(country))
+
+countrycode= 91
+print("I live in",countrycode, "which is a very good place")
+
+print("I live in",str(countrycode), "which is a very good place")
+
+print("I live in {0} which is a very good place whose pin is {1}".format(country, countrycode))
+
+a=1.11
+
+b=int(a)
+
+c=float(b)
+
+x=2
+y=3
+
+z=x+y
+
+print(z)
+print(x)
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+
+x=5
+x=x-1
+x
+
+x-=1
+x
+
+x*=2
+x
+
+y=x**2
+y
+
+y=x**3
+y
+
+x**=2
+x
+
+a=10
+b=11
+
+a/2
+b/2
+
+#moduLo
+a%2
+b%2
+
+#boolean
+'''
+0-9
+deciamal 
+
+boolean
+0-1
+false-true
+And
+Or 
+Not
+
+And
+A B X
+0 0 0
+1 1 1
+1 0 0
+0 1 0
+
+Or
+A B X
+0 0 0
+1 1 1
+1 0 1
+0 1 1
+
+Not
+A X
+0 1
+1 0
+'''
+
+
+'''AND Function'''
+A=0
+B=1
+
+X= A and B
+X
+
+A=True
+B=False
+
+X=A and B
+X
+
+A=True
+B=True
+
+X=A and B
+X
+
+'''OR Function'''
+
+A=0
+B=1
+
+X= A or B
+X
+
+A=True
+B=False
+
+X=A or B
+X
+
+A=True
+B=True
+
+X=A or B
+X
+
+type(X)
+
+'''NOT Function'''
+
+A=0
+X=not(A)
+X
+
+A=1
+X=not(A)
+X
+
+A=15
+bool(A)
+
+B=0
+bool(B)
+
+Fname="Kirat"
+Lname="Chhabra"
+RollNo= 11
+name= Fname + Lname
+name
+
+name= Fname +" " + Lname
+name
+
+name= Fname +" " + Lname + RollNo
+name
+
+name= Fname +" " + Lname + str(RollNo)
+name
+
+len(Fname)
+
+len(name)
+
+Lname=Lname.capitalize()
+print(Lname)
+
+Lname=Lname.upper()
+print(Lname)
+
+Lname=Lname.lower()
+print(Lname)
+
+Lname=Lname.rjust(10)
+print(Lname)
+
+Lname=Lname.center(15)
+print(Lname)
+
+name
+name=name.replace("11", "12")
+name
